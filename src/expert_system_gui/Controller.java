76b0419ee465ae022b8521378e689974443eb26f
Piotr <piotrmeller95@gmailc.om>
2018-02-23 05:14:02 +0100

    update

--- src/expert_system_gui/Controller.java
+++ src/expert_system_gui/Controller.java
@@ -2,54 +2,58 @@ package expert_system_gui;
 
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableArray;
-import javafx.collections.ObservableList;
+import javafx.collections.ListChangeListener;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
-import javafx.stage.Stage;
 import javafx.stage.Window;
-import org.semanticweb.owlapi.model.*;
 
 import java.io.File;
-import java.util.ArrayList;
+import java.util.List;
+
 
 public class Controller
 {
 
     @FXML
     TreeView<String> classHierarchy;
-
     @FXML
     AnchorPane MainWindow;
-
     @FXML
     ListView<String> ObjectPropertiesList;
-
     @FXML
     ListView<String> DataPropertiesList;
-
+    @FXML
+    ListView<String> IndividualList;
     @FXML
     ComboBox<String> ComboBoxSelectObjectProperty;
-
     @FXML
     ComboBox<String> ComboBoxSelectDataProperty;
-
     @FXML
     ComboBox<String> ComboBoxSelectClass;
-
     @FXML
     Button ButtonResetObjectProperty;
-
     @FXML
     Button ButtonAddObjectProperty;
+    @FXML
+    TextField TextFieldDataProperty;
+    @FXML
+    ComboBox<String> ComboBoxRestriction;
+    @FXML
+    Button ButtonResetDataProperty, ButtonAddDataProperty;
 
 
     public void initialize()
     {
         // classHierarchy.setRoot(new TreeItem<>("Thing"));
+        TextFieldDataProperty.textProperty().addListener((observable, oldValue, newValue) ->
+        {
+            if (!newValue.matches("\\d*"))
+                TextFieldDataProperty.setText(oldValue);
+        });
+
     }
 
     @FXML
@@ -66,7 +70,7 @@ public class Controller
 
         if (file != null)
         {
-            if (file.exists() == true)
+            if (file.exists())
             {
                 try
                 {
@@ -86,12 +90,15 @@ public class Controller
 
                 Main.getInstance().ontology.printOntologyToTreeView(rootItem);
 
-                ArrayList<String> list = Main.getInstance().ontology.listOfObjectProperties();
-
-                ObservableList<String> ObjectPropertiesList = FXCollections.observableArrayList(list);
-
-                ComboBoxSelectObjectProperty.setItems(ObjectPropertiesList);
-
+                ComboBoxSelectObjectProperty.setItems(
+                        FXCollections.observableArrayList(
+                                Main.getInstance().ontology.listOfObjectProperties()));
+                ComboBoxSelectDataProperty.setItems(
+                        FXCollections.observableArrayList(
+                                Main.getInstance().ontology.listOfDataProperties()));
+                IndividualList.setItems(
+                        FXCollections.observableArrayList(
+                                Main.getInstance().ontology.listOfIndividuals()));
 
             } else
             {
@@ -105,26 +112,115 @@ public class Controller
     }
 
     @FXML
+    private void saveOntologyClick(ActionEvent event)
+    {
+        try
+        {
+            Main.getInstance().ontology.saveOntology();
+        } catch (Throwable e)
+        {
+
+        }
+    }
+
+    @FXML
     private void ObjectPropertySelected(ActionEvent event)
     {
         String property = ComboBoxSelectObjectProperty.getValue();
-        ArrayList<String> list = Main.getInstance().ontology.listOfClassesInRangeOfObjectProperties(property);
-        ObservableList<String> inRange = FXCollections.observableArrayList(list);
-        ComboBoxSelectClass.setItems(inRange);
+        if (property != null)
+        {
+            ComboBoxSelectClass.setItems(
+                    FXCollections.observableArrayList(
+                            Main.getInstance().ontology.listOfClassesInRangeOfObjectProperties(property)));
+        }
+    }
+
+
+    @FXML
+    private void DataPropertySelected(ActionEvent event)
+    {
+        String property = ComboBoxSelectDataProperty.getValue();
+        if (property != null)
+        {
+            if (Main.getInstance().ontology.hasDataPropertyNumericRange(property))
+            {
+                ComboBoxRestriction.getItems().clear();
+                ComboBoxRestriction.getItems().add("=");
+                ComboBoxRestriction.getItems().add(">=");
+                ComboBoxRestriction.getItems().add("<=");
+                ComboBoxRestriction.getItems().add(">");
+                ComboBoxRestriction.getItems().add("<");
+
+                ComboBoxRestriction.getSelectionModel().select(0);
+            }
+        }
     }
 
     @FXML
     private void ButtonResetObjectPropertyClick(ActionEvent event)
     {
         ObjectPropertiesList.setItems(FXCollections.observableArrayList());
+        ComboBoxSelectObjectProperty.getSelectionModel().clearSelection();
+        ComboBoxSelectClass.getSelectionModel().clearSelection();
+
+        if (Main.getInstance().ontology.getOntology() != null)
+            ComboBoxSelectObjectProperty.setItems(
+                    FXCollections.observableArrayList(
+                            Main.getInstance().ontology.listOfObjectProperties()));
+
+        if (ObjectPropertiesList.getItems().isEmpty() && DataPropertiesList.getItems().isEmpty())
+            IndividualList.setItems(
+                    FXCollections.observableArrayList(
+                            Main.getInstance().ontology.listOfIndividuals()));
     }
 
     @FXML
     private void ButtonAddObjectPropertyClick(ActionEvent event)
     {
         if (ComboBoxSelectClass.getValue() != null && ComboBoxSelectObjectProperty.getValue() != null)
+        {
             ObjectPropertiesList.getItems().add(ComboBoxSelectObjectProperty.getValue() + " " + ComboBoxSelectClass.getValue());
+            ComboBoxSelectObjectProperty.getItems().remove(ComboBoxSelectObjectProperty.getValue());
+            ComboBoxSelectObjectProperty.getSelectionModel().select(0);
+
+            List<String> list = Main.getInstance().ontology.listOfInstancesForSpecifiedData(ObjectPropertiesList.getItems(), DataPropertiesList.getItems());
+            IndividualList.setItems(FXCollections.observableArrayList(list));
+        }
     }
+
+    @FXML
+    private void ButtonAddDataPropertyClick(ActionEvent event)
+    {
+        if (ComboBoxRestriction.getValue() != null && ComboBoxSelectDataProperty.getValue() != null && TextFieldDataProperty.getText() != null)
+        {
+            DataPropertiesList.getItems().add(ComboBoxSelectDataProperty.getValue() + " " + ComboBoxRestriction.getValue() + " " + TextFieldDataProperty.getText());
+            ComboBoxSelectDataProperty.getItems().remove(ComboBoxSelectDataProperty.getValue());
+            ComboBoxSelectDataProperty.getSelectionModel().select(0);
+
+            List<String> list = Main.getInstance().ontology.listOfInstancesForSpecifiedData(ObjectPropertiesList.getItems(), DataPropertiesList.getItems());
+            IndividualList.setItems(FXCollections.observableArrayList(list));
+        }
+    }
+
+    @FXML
+    private void ButtonResetDataPropertyClick(ActionEvent event)
+    {
+        DataPropertiesList.setItems(FXCollections.observableArrayList());
+        ComboBoxSelectDataProperty.getSelectionModel().clearSelection();
+        ComboBoxRestriction.getSelectionModel().clearSelection();
+        TextFieldDataProperty.clear();
+
+        if (Main.getInstance().ontology.getOntology() != null)
+            ComboBoxSelectDataProperty.setItems(
+                    FXCollections.observableArrayList(
+                            Main.getInstance().ontology.listOfDataProperties()));
+
+        if (ObjectPropertiesList.getItems().isEmpty() && DataPropertiesList.getItems().isEmpty())
+            IndividualList.setItems(
+                    FXCollections.observableArrayList(
+                            Main.getInstance().ontology.listOfIndividuals()));
+    }
+
 
     @FXML
     private void exitClick(ActionEvent event)
--- src/expert_system_gui/Ontology.java
+++ src/expert_system_gui/Ontology.java
@@ -1,9 +1,9 @@
 package expert_system_gui;
 
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
 import javafx.scene.control.TreeItem;
+import org.semanticweb.HermiT.Reasoner;
+import org.semanticweb.HermiT.ReasonerFactory;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.io.OWLParserException;
 import org.semanticweb.owlapi.model.*;
@@ -11,11 +11,14 @@ import org.semanticweb.owlapi.reasoner.OWLReasoner;
 import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
 import org.semanticweb.owlapi.reasoner.structural.StructuralReasonerFactory;
 import org.semanticweb.owlapi.util.ShortFormProvider;
+import org.semanticweb.owlapi.vocab.OWLFacet;
+import org.semarglproject.vocab.OWL;
 import uk.ac.manchester.cs.jfact.*;
-import uk.ac.manchester.cs.owl.owlapi.OWLObjectPropertyImpl;
+import uk.ac.manchester.cs.owl.owlapi.OWLLiteralImpl;
 
 import java.io.File;
-import java.util.ArrayList;
+import java.io.IOException;
+import java.util.*;
 
 
 public class Ontology
@@ -40,7 +43,7 @@ public class Ontology
     {
         ontologyManager = OWLManager.createOWLOntologyManager();
         dataFactory = ontologyManager.getOWLDataFactory();
-        reasonerFactory = new JFactFactory();
+        //reasonerFactory = new JFactFactory();
     }
 
 
@@ -50,9 +53,19 @@ public class Ontology
             ontologyManager.removeOntology(ontology);
 
         ontology = ontologyManager.loadOntologyFromOntologyDocument(file);
-        ontologyIRI = ontology.getOntologyID().getOntologyIRI().get();
-        reasoner = reasonerFactory.createReasoner(ontology);
+        if (ontology.getOntologyID().getOntologyIRI().isPresent())
+            ontologyIRI = ontology.getOntologyID().getOntologyIRI().get();
+        ///reasoner = reasonerFactory.createReasoner(ontology);
         thing = ontologyManager.getOWLDataFactory().getOWLThing();
+
+    }
+
+    public void saveOntology() throws OWLOntologyStorageException, OWLOntologyCreationException, IOException
+    {
+        if(ontology!=null)
+        {
+            ontologyManager.saveOntology(ontology);
+        }
     }
 
     public void printOntologyToTreeView(TreeItem<String> root)
@@ -72,7 +85,8 @@ public class Ontology
             {
                 if (!child.equals(cl) && reasoner.isSatisfiable(child))
                 {
-                    root.getChildren().add(new TreeItem<>(child.getIRI().getShortForm()));
+                    String str = getShortForm(child.getIRI());
+                    root.getChildren().add(new TreeItem<>(str));
                     printOntologyToTreeView(root.getChildren().get(i), child);
                     i++;
 
@@ -81,7 +95,7 @@ public class Ontology
         }
     }
 
-    public ArrayList<String> listOfObjectProperties()
+    public List<String> listOfObjectProperties()
     {
         reasonerFactory = new StructuralReasonerFactory();
         reasoner = reasonerFactory.createReasoner(ontology);
@@ -94,7 +108,8 @@ public class Ontology
         {
             if (!reasoner.getSubObjectProperties(child).isEmpty() && reasoner.getSubObjectProperties(child, true).containsEntity(bottomProperty))
             {
-                String str = child.getNamedProperty().getIRI().getShortForm();
+                //String str = child.getNamedProperty().getIRI().getShortForm();
+                String str = getShortForm(child.getNamedProperty().getIRI());
                 if (!list.contains(str))
                     list.add(str);
             }
@@ -104,9 +119,39 @@ public class Ontology
         return list;
     }
 
-    public ArrayList<String> listOfClassesInRangeOfObjectProperties(String ObjectPropertyShortForm)
+    public List<String> listOfDataProperties()
     {
-        reasonerFactory =new StructuralReasonerFactory();// new JFactFactory();
+        reasonerFactory = new StructuralReasonerFactory();
+        reasoner = reasonerFactory.createReasoner(ontology);
+
+        OWLDataProperty topProperty = ontologyManager.getOWLDataFactory().getOWLTopDataProperty();
+        OWLDataProperty bottomProperty = ontologyManager.getOWLDataFactory().getOWLBottomDataProperty();
+        ArrayList<String> list = new ArrayList<>();
+
+        for (OWLDataProperty child : reasoner.getSubDataProperties(topProperty, false).getFlattened())
+        {
+            if (!reasoner.getSubDataProperties(child).isEmpty() && reasoner.getSubDataProperties(child, true).containsEntity(bottomProperty))
+            {
+                //String str = child.getNamedProperty().getIRI().getShortForm();
+                String str = getShortForm(child.getIRI());
+                if (!list.contains(str))
+                    list.add(str);
+            }
+        }
+
+        reasoner.dispose();
+        return list;
+    }
+
+    public List<String> listOfObjectPropertiesOfIndividual(String Individual)
+    {
+        return null;
+    }
+
+
+    public List<String> listOfClassesInRangeOfObjectProperties(String ObjectPropertyShortForm)
+    {
+        reasonerFactory = new StructuralReasonerFactory();// new JFactFactory();
         reasoner = reasonerFactory.createReasoner(ontology);
         IRI propertyIRI = IRI.create(ontologyIRI.getIRIString() + "#" + ObjectPropertyShortForm);
         ArrayList<String> list = new ArrayList<>();
@@ -118,21 +163,155 @@ public class Ontology
 
         for (OWLClass child : reasoner.getObjectPropertyRanges(property, true).getFlattened())
         {
-
-
-            for(OWLClass child2 : reasoner.getSubClasses(child,false).getFlattened())
+            for (OWLClass child2 : reasoner.getSubClasses(child, false).getFlattened())
             {
-                 if (!reasoner.getSubClasses(child2).isEmpty() && reasoner.getSubClasses(child2, true).containsEntity(Nothing))
-                 {
-                String str = child2.getIRI().getShortForm();
-                 if (!list.contains(str))
-                list.add(str);
-                 }
+                if (!reasoner.getSubClasses(child2).isEmpty() && reasoner.getSubClasses(child2, true).containsEntity(Nothing))
+                {
+                    //String str = child2.getIRI().getShortForm();
+                    String str = getShortForm(child2.getIRI());
+                    if (!list.contains(str))
+                        list.add(str);
+                }
             }
+        }
+        reasoner.dispose();
+        return list;
+    }
+
+    public List<String> listOfIndividuals()
+    {
+        reasonerFactory = new StructuralReasonerFactory();// new JFactFactory();
+        reasoner = reasonerFactory.createReasoner(ontology);
+
+        ArrayList<String> list = new ArrayList<>();
+        for (OWLNamedIndividual ind : ontology.getIndividualsInSignature())
+        {
+            list.add(getShortForm(ind.getIRI()));
+        }
+        return list;
+    }
+
+    public List<String> listOfInstancesForSpecifiedData(List<String> ObjectPropertiesList, List<String> DataPropertiesList)
+    {
+        OWLClass temp = ontologyManager.getOWLDataFactory().getOWLClass(ontologyIRI + "#" + "temp");
+        Set<OWLClassExpression> arguments = new HashSet<>();
+        //arguments.add(temp);
+
+        for (String str : ObjectPropertiesList)
+        {
+            String[] splitStr = str.trim().split("\\s+");//0 - property, 1 - class
+
+            OWLObjectProperty property = ontologyManager.getOWLDataFactory().getOWLObjectProperty(ontologyIRI + "#" + splitStr[0]);
+            OWLClass cl = ontologyManager.getOWLDataFactory().getOWLClass(ontologyIRI + "#" + splitStr[1]);
+
+            arguments.add(dataFactory.getOWLObjectAllValuesFrom(property, cl));
+
+        }
+
+        for (String str : DataPropertiesList)
+        {
+            String[] splitStr = str.trim().split("\\s+");//0 - property, 1 - restriction, 2 - value
+
+            int value = Integer.parseInt(splitStr[2]);
+            OWLDataProperty property = ontologyManager.getOWLDataFactory().getOWLDataProperty(getEntityIRI(splitStr[0]));
+            OWLDatatype dataType = getOWLDatatype(splitStr[0]);
+            Set<OWLFacetRestriction> facetRestriction = new HashSet<>();
+            switch (splitStr[1])
+            {
+                case ">":
+                    facetRestriction.add(ontologyManager.getOWLDataFactory().getOWLFacetRestriction(OWLFacet.MIN_EXCLUSIVE, value));
+                    break;
+                case "<":
+                    facetRestriction.add(ontologyManager.getOWLDataFactory().getOWLFacetRestriction(OWLFacet.MAX_EXCLUSIVE, value));
+                    break;
+                case ">=":
+                    facetRestriction.add(ontologyManager.getOWLDataFactory().getOWLFacetRestriction(OWLFacet.MIN_INCLUSIVE, value));
+                    break;
+                case "<=":
+                    facetRestriction.add(ontologyManager.getOWLDataFactory().getOWLFacetRestriction(OWLFacet.MAX_INCLUSIVE, value));
+                    break;
+                case "=":
+                    facetRestriction.add(ontologyManager.getOWLDataFactory().getOWLFacetRestriction(OWLFacet.MIN_INCLUSIVE, value));
+                    facetRestriction.add(ontologyManager.getOWLDataFactory().getOWLFacetRestriction(OWLFacet.MAX_INCLUSIVE, value));
+                    break;
+            }
+
+            OWLDatatypeRestriction restriction = ontologyManager.getOWLDataFactory().getOWLDatatypeRestriction(dataType,
+                    facetRestriction);
+
+            arguments.add(dataFactory.getOWLDataSomeValuesFrom(property, restriction));
+
+        }
+        OWLClassExpression expr = ontologyManager.getOWLDataFactory().getOWLObjectIntersectionOf(arguments);
+        OWLAxiom tempAxiom = dataFactory.getOWLEquivalentClassesAxiom(temp, expr);
+
+        ontologyManager.addAxiom(ontology, tempAxiom);
+
+        //reasonerFactory = new JFactFactory(); //I don't know why but Fact++ is not working for Data Properties
+        reasonerFactory = new ReasonerFactory();
+        reasoner = reasonerFactory.createReasoner(ontology);
+
+        ArrayList<String> list = new ArrayList<>();
+
+
+        for (OWLNamedIndividual ind : reasoner.getInstances(temp, true).getFlattened())
+        {
+            list.add(getShortForm(ind.getIRI()));
         }
 
 
+        ontology.removeAxiom(tempAxiom);
+
+        reasoner.dispose();
         return list;
+    }
+
+
+    public List<String> listOfInstancesOfClass(OWLClass cl)
+    {
+        reasonerFactory = new JFactFactory();
+        reasoner = reasonerFactory.createReasoner(ontology);
+
+        ArrayList<String> list = new ArrayList<>();
+
+        for (OWLNamedIndividual ind : reasoner.getInstances(cl, true).getFlattened())
+        {
+            list.add(getShortForm(ind.getIRI()));
+        }
+
+        reasoner.dispose();
+        return list;
+    }
+
+    public boolean hasDataPropertyNumericRange(String property)
+    {
+        OWLDatatype data = getOWLDatatype(property);
+        return (data.isInteger() || data.isFloat() || data.isBoolean());
+    }
+
+    private OWLDatatype getOWLDatatype(String dataProperty)
+    {
+        OWLDataProperty property = ontologyManager.getOWLDataFactory().getOWLDataProperty(getEntityIRI(dataProperty));
+        Set<OWLDatatype> dataTypes = new HashSet<>();
+        //long line
+        ontology.dataPropertyRangeAxioms(property).forEach(owlDataPropertyRangeAxiom -> owlDataPropertyRangeAxiom.getRange().datatypesInSignature().forEach(owlDatatype -> dataTypes.add(owlDatatype)));
+        OWLDatatype out = null;
+        for (OWLDatatype data : dataTypes)
+        {
+            out = data;
+        }
+        return out;
+    }
+
+    private String getShortForm(IRI objectIRI)  //original getShortForm() is removing numbers
+    {
+        String str = objectIRI.getIRIString();
+        return str.replaceAll(ontologyIRI.getIRIString() + "#", "");
+    }
+
+    private IRI getEntityIRI(String entity)
+    {
+        return IRI.create(ontologyIRI + "#" + entity);
     }
 
 
